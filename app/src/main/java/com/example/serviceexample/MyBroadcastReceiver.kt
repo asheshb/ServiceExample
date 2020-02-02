17a99a215efaeb188ffc6e7612b0df7119f68ba7
Ashesh Bharadwaj <ppashesh@gmail.com>
2020-02-02 21:53:09 +0530

    Initial Commit

--- app/src/main/java/com/example/serviceexample/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/serviceexample/MyBroadcastReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,30 +15,30 @@ private const val TAG = "MyBroadcastReceiver"
 class MyBroadcastReceiver : BroadcastReceiver() {
     private var pendingResult: PendingResult? = null
 
-//    override fun onReceive(context: Context, intent: Intent) {
-//        if(intent.action == Intent.ACTION_LOCALE_CHANGED){
-//            //runs on main thread
-//            Toast.makeText(context, Intent.ACTION_LOCALE_CHANGED, Toast.LENGTH_LONG).show()
-//        }
-//    }
-
-    //region
     override fun onReceive(context: Context, intent: Intent) {
         if(intent.action == Intent.ACTION_LOCALE_CHANGED){
-            GlobalScope.launch {
-               showToast(context)
-           }
-        }
-        //if need more time to finish. Still we are expected to finish under 10 seconds
-        pendingResult = goAsync()
-    }
-
-    private suspend fun showToast(context: Context){
-        withContext(Dispatchers.Main){
+            //runs on main thread
             Toast.makeText(context, Intent.ACTION_LOCALE_CHANGED, Toast.LENGTH_LONG).show()
         }
-
-        pendingResult?.finish()
     }
+
+    //region
+//    override fun onReceive(context: Context, intent: Intent) {
+//        if(intent.action == Intent.ACTION_LOCALE_CHANGED){
+//            GlobalScope.launch {
+//               showToast(context)
+//           }
+//        }
+//        //if need more time to finish. Still we are expected to finish under 10 seconds
+//        pendingResult = goAsync()
+//    }
+//
+//    private suspend fun showToast(context: Context){
+//        withContext(Dispatchers.Main){
+//            Toast.makeText(context, Intent.ACTION_LOCALE_CHANGED, Toast.LENGTH_LONG).show()
+//        }
+//
+//        pendingResult?.finish()
+//    }
     //endregion
 }
