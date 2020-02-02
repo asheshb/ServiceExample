527d6539480871cea7c5175ab865ce71f2a8acb9
Ashesh Bharadwaj <ppashesh@gmail.com>
2020-02-02 21:51:10 +0530

    Initial Commit

--- app/src/main/java/com/example/serviceexample/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/serviceexample/MyBroadcastReceiver.kt
@@ -4,13 +4,40 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val TAG = "MyBroadcastReceiver"
 
 class MyBroadcastReceiver : BroadcastReceiver() {
+    private var pendingResult: PendingResult? = null
 
+//    override fun onReceive(context: Context, intent: Intent) {
+//        if(intent.action == Intent.ACTION_LOCALE_CHANGED){
+//            //runs on main thread
+//            Toast.makeText(context, Intent.ACTION_LOCALE_CHANGED, Toast.LENGTH_LONG).show()
+//        }
+//    }
+
+    //region
     override fun onReceive(context: Context, intent: Intent) {
-        //runs on Main thread
-        if (intent.action == Intent.ACTION_LOCALE_CHANGED) {
-             Toast.makeText(context, Intent.ACTION_LOCALE_CHANGED, Toast.LENGTH_LONG).show()
-         }
+        if(intent.action == Intent.ACTION_LOCALE_CHANGED){
+            GlobalScope.launch {
+               showToast(context)
+           }
+        }
+        //if need more time to finish. Still we are expected to finish under 10 seconds
+        pendingResult = goAsync()
     }
+
+    private suspend fun showToast(context: Context){
+        withContext(Dispatchers.Main){
+            Toast.makeText(context, Intent.ACTION_LOCALE_CHANGED, Toast.LENGTH_LONG).show()
+        }
+
+        pendingResult?.finish()
+    }
+    //endregion
 }
